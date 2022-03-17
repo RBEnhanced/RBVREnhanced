2b65bcad0c15f5c00ce323a600f01f6741307da3
InvoxiPlayGames <webmaster@invoxiplaygames.uk>
2022-03-17 00:41:44 +0000

    add oculus controller hook and custom song enumeration

--- RBVREnhanced/dllmain.cpp
+++ RBVREnhanced/dllmain.cpp
@@ -11,8 +11,15 @@
 #include <string>
 #include <filesystem>
 
+// namespace shortcuts
+namespace fs = std::filesystem;
+
 // definitions
+// folder to load rawfiles from by default
 #define DEFAULT_RAWFILES_DIR "rawfiles"
+// folder to load songs from by default - TODO: add songs folder
+#define DEFAULT_SONGS_DIR "."
+// console log message macros
 #define RBVRE_MSG(x, ...) std::printf("[RBVRE:MSG] " x "\n", __VA_ARGS__)
 #ifdef _DEBUG
 #define RBVRE_DEBUG(x, ...) std::printf("[RBVRE:DBG] " x "\n", __VA_ARGS__)
@@ -20,10 +27,15 @@
 #define RBVRE_DEBUG(x, ...) 
 #endif // _DEBUG
 
-// global variables
+// global variables - config options
 std::string RawfilesFolder(DEFAULT_RAWFILES_DIR);
+std::string SongsFolder(DEFAULT_SONGS_DIR);
 bool PrintRawfiles = false;
 bool PrintArkfiles = false;
+// global variables - song loading
+std::vector<std::string> skuList;
+int loadedExtraSKUs = 0;
+int loadedOfficialSKUs = 0;
 
 // ARKless file loading hook
 typedef enum _FileMode {
@@ -40,32 +52,109 @@ void* NewFileHook(const char* path, FileMode mode) {
     if (rawpath.crbegin()[0] != '/') rawpath.append("/");
     rawpath.append(path);
     const char* newpath = rawpath.c_str();
-    if (std::filesystem::exists(newpath)) {
+    if (fs::exists(newpath)) {
         if (PrintRawfiles) RBVRE_MSG("Loading rawfile: %s", newpath);
         return NewFileTrampoline(newpath, kReadNoArk);
     }
-    if (PrintArkfiles) RBVRE_MSG("Loading ARK file: %s", path);
+    /* // TODO: songs subfolder
+    rawpath.replace(0, RawfilesFolder.length(), SongsFolder);
+    newpath = rawpath.c_str();
+    if (fs::exists(newpath)) {
+        if (PrintRawfiles) RBVRE_MSG("Loading rawfile: %s", newpath);
+        return NewFileTrampoline(newpath, kReadNoArk);
+    }*/
+    if (PrintArkfiles || (PrintRawfiles && mode == kReadNoArk)) RBVRE_MSG("Loading file: %s", path);
     return NewFileTrampoline(path, mode);
 }
+bool (*FileExistsTrampoline)(const char* path, FileMode mode);
+bool FileExistsHook(const char* path, FileMode mode) {
+    std::string rawpath(RawfilesFolder);
+    if (rawpath.crbegin()[0] != '/') rawpath.append("/");
+    rawpath.append(path);
+    const char* newpath = rawpath.c_str();
+    if (fs::exists(newpath)) {
+        return FileExistsTrampoline(newpath, kReadNoArk);
+    }
+    /* // TODO: songs subfolder
+    rawpath.replace(0, RawfilesFolder.length(), SongsFolder);
+    newpath = rawpath.c_str();
+    if (fs::exists(newpath)) {
+        return FileExistsTrampoline(newpath, kReadNoArk);
+    }*/
+    return FileExistsTrampoline(path, mode);
+}
 
-// SKU song name hook, for debug purposes
+// controller hook to remove need for having an oculus controller connected
+bool (*OculusControllerConnectedTrampoline)(void* OculusController, int HandType);
+bool OculusControllerConnectedHook(void* OculusController, int HandType) {
+    if (HandType == 1) return true; // right hand always true
+    return OculusControllerConnectedTrampoline(OculusController, HandType);
+}
+
+// DLC loading hooks to add custom songs into the array
+#define CustomElemMask 0xFF000000
 void* (*SkuToSongNameTrampoline)(void* unk, const char* SKU);
 void* SkuToSongNameHook(void * unk, const char* SKU) {
     RBVRE_DEBUG("SKU: %s", SKU);
     return SkuToSongNameTrampoline(unk, SKU);
 }
+int (*PurchaseArrayGetSizeTrampoline)(void* parr);
+int PurchaseArrayGetSizeHook(void* parr) {
+    loadedOfficialSKUs = PurchaseArrayGetSizeTrampoline(parr);
+    return loadedOfficialSKUs + loadedExtraSKUs;
+}
+void *(*PurchaseArrayGetElementTrampoline)(void* parr, int elemNum);
+void* PurchaseArrayGetElementHook(void* parr, int elemNum) {
+    if (elemNum < loadedOfficialSKUs) return PurchaseArrayGetElementTrampoline(parr, elemNum);
+    return (void*)(CustomElemMask | elemNum);
+}
+const char* (*PurchaseGetSKUTrampoline)(void* pelem);
+const char* PurchaseGetSKUHook(void* pelem) {
+    if (((int)pelem & CustomElemMask) == CustomElemMask) {
+        int realSKU = ((int)pelem & ~CustomElemMask) - loadedOfficialSKUs;
+        return skuList[realSKU].c_str();
+    }
+    return PurchaseGetSKUTrampoline(pelem);
+}
+
+// scan through a directory to get the list of ARKs
+void ScanSongs(fs::path directory) {
+    for (const auto& entry : fs::directory_iterator(directory)) {
+        if (entry.is_directory()) {
+            // ignore subdirectories for now, TODO: scan through subdirectories and handle them
+            //ScanSongs(entry.path());
+        } else if (entry.is_regular_file() && // is a regular file
+            entry.path().extension().compare(".hdr") == 0 && // is a HDR file
+            entry.path().stem().compare("main_pc") != 0) { // is *not* the main_pc file
+            // create SKU and add to the list
+            std::string SKU("song_");
+            SKU.append(entry.path().stem().string());
+            SKU.resize(SKU.size() - 3); // remove _pc suffix
+            skuList.push_back(SKU);
+        }
+    }
+}
 
 // initialisation function
 void InitMod() {
-    // install hooks
+    // install game hooks
     InstallHook((void*)0x1406fa9a0, &NewFileHook, (void**)&NewFileTrampoline);
+    InstallHook((void*)0x1406f9380, &FileExistsHook, (void**)&FileExistsTrampoline);
     InstallHook((void*)0x1402e0560, &SkuToSongNameHook, (void**)&SkuToSongNameTrampoline);
+    InstallHook((void*)0x140bb98d0, &OculusControllerConnectedHook, (void**)&OculusControllerConnectedTrampoline);
+    // install oculus API hooks for song loading purposes
+    HMODULE OVRPlatform = GetModuleHandleA("LibOVRPlatform64_1.dll");
+    if (OVRPlatform != NULL) {
+        InstallHook((void*)GetProcAddress(OVRPlatform, "ovr_PurchaseArray_GetSize"), &PurchaseArrayGetSizeHook, (void**)&PurchaseArrayGetSizeTrampoline);
+        InstallHook((void*)GetProcAddress(OVRPlatform, "ovr_PurchaseArray_GetElement"), &PurchaseArrayGetElementHook, (void**)&PurchaseArrayGetElementTrampoline);
+        InstallHook((void*)GetProcAddress(OVRPlatform, "ovr_Purchase_GetSKU"), &PurchaseGetSKUHook, (void**)&PurchaseGetSKUTrampoline);
+    }
     // load config
     INIReader reader("RBVREnhanced.ini");
     RawfilesFolder = reader.Get("Arkless", "RawfilesFolder", DEFAULT_RAWFILES_DIR);
     PrintRawfiles = reader.GetBoolean("Arkless", "PrintRawfiles", false);
     PrintArkfiles = reader.GetBoolean("Arkless", "PrintArkfiles", false);
-    if (reader.GetBoolean("Settings", "DebugConsole", true)) {
+    if (reader.GetBoolean("Settings", "DebugConsole", false)) {
         // allocate console
         AllocConsole();
         SetConsoleTitleA("RBVREnhanced");
@@ -74,6 +163,10 @@ void InitMod() {
         freopen_s(&fDummy, "CONOUT$", "w", stderr);
         freopen_s(&fDummy, "CONOUT$", "w", stdout);
     }
+    // scan for songs in the specified folder
+    ScanSongs(SongsFolder);
+    loadedExtraSKUs = skuList.size();
+    RBVRE_MSG("Loaded %i songs!", loadedExtraSKUs);
     // ready!
     RBVRE_MSG("Ready!");
 }
--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 
 A work-in-progress mod for Rock Band VR. More details to come!
 
+Join the [RBEnhanced Discord guild](https://discord.gg/6rRUWXPYwb) for discussion and assistance.
+
+## Features
+
+* ARKless file loading
+* Removes controller requirement
+* Automatic detection of custom songs
+    * TODO: split songs into "songs" subdirectory.
+
 ## Injection
 
 Either add the DLL to the imports table using a tool such as StudPE, or use something like [simpledllloader](https://github.com/aap/simpledllloader).
@@ -9,5 +18,8 @@ Either add the DLL to the imports table using a tool such as StudPE, or use some
 ## Credits
 
 * inih / INIReader: https://github.com/benhoyt/inih
+    * Used under the MIT license.
 * Capstone: https://github.com/capstone-engine/capstone
+    * Used under the 3-clause BSD license.
 * Hooking By Example: https://github.com/khalladay/hooking-by-example
+    * Used under the MIT license.
